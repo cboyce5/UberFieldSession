347addfb873fd4e81468e47d86e33cad88b75dd5
Cliff Boyce <cboyce@mines.edu>
2016-06-04 22:37:34 -0600

    Added changing of features to java client

--- java/src/geospatial/client/JavaClient.java
+++ java/src/geospatial/client/JavaClient.java
@@ -21,7 +21,7 @@ public class JavaClient {
 	
 	private Feature currentFeature;
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 
 		if (args.length != 1) {
 			System.out.println("Please enter 'simple' or 'secure'");
@@ -77,12 +77,19 @@ public class JavaClient {
 				Point newPoint = new Point(x,y);
 				System.out.print("Enter the new payload: ");
 				String newPayload = reader.nextLine();
+				System.out.println("Your new feature is now your current feature.");
 				currentFeature = client.createFeature(newPoint, newPayload);
 				break;
 			case 2:
 				System.out.print("Enter the feature id: ");
 				String id = reader.nextLine();
 				currentFeature = client.getFeature(id);
+				if (currentFeature != null) {
+					System.out.println("Your current feature is now saved.");
+				}
+				else {
+					System.out.println("That feature does not exist.");
+				}
 				break;
 			case 3:
 				System.out.print("Enter the top left x: ");
@@ -100,11 +107,58 @@ public class JavaClient {
 				break;
 			case 4:
 				boolean saveSuccess = client.saveFeature(currentFeature);
+				if (saveSuccess) {
+					System.out.println("Your feature was saved successfully.");
+				}
+				else {
+					System.out.println("Your feature was saved unsuccessfully.");
+				}
 				break;
 			case 5:
 				boolean deleteSuccess = client.deleteFeature(currentFeature);
+				if (deleteSuccess) {
+					System.out.println("Your feature was deleted successfully.");
+				}
+				else {
+					System.out.println("Your feature was deleted unsuccessfully.");
+				}
 				break;
 			case 6:
+				printFeatureChangeOptions();
+				int option = reader.nextInt();
+				switch (option) {
+				case 1:
+					System.out.print("Enter the new point x: ");
+					double newX = reader.nextDouble();
+					System.out.print("Enter the new point y: ");
+					double newY = reader.nextDouble();
+					Point newPoint = new Point(newX,newY);
+					currentFeature = curentFeature.setPoint(newPoint);
+					break;
+				case 2:
+					System.out.print("Enter the new state (1 or 2): ");
+					int newState = reader.nextInt();
+					if (newState == 1) {
+						currentFeature = currentFeature.setState(FeatureState.CLEAN);
+					}
+					else if (newState == 2){
+						currentFeature = currentFeature.setState(FeatureState.DIRTY);
+					}
+					else {
+						System.out.println("Please enter a valid state.");
+					}
+					break;
+				case 3:
+					System.out.print("Enter the new payload: ");
+					String newPayload = reader.nextLine();
+					currentFeature = currentFeature.setPayload(newPayload);
+					break;
+				default:
+					System.out.println("Enter a valid option.");
+					break;
+				}
+				break;
+			case 7:
 				loop = false;
 				break;
 			default:
@@ -115,13 +169,22 @@ public class JavaClient {
 	}
 	
 	private static void printOptions() {
-		System.out.println("Options: ");
+		System.out.println("Options:");
 		System.out.println("1. Create Feature");
 		System.out.println("2. Get Feature");
 		System.out.println("3. Get Features in Rectangle");
 		System.out.println("4. Save Feature");
 		System.out.println("5. Delete Feature");
-		System.out.println("6. Exit");
-		System.out.println("Enter your selection: ");
+		System.out.println("6. Edit Feature");
+		System.out.println("7. Exit");
+		System.out.print("Enter your selection: ");
+	}
+	
+	private static void printFeatureChangeOptions() {
+		System.out.println("Options:");
+		System.out.println("1. Change Point");
+		System.out.println("2. Change State");
+		System.out.println("3. Change Payload");
+		System.out.print("Enter your selection: ");
 	}
 }
